3d4d43c134c69a60e96e2959ae980179ceafd447
Artemartem345 <popovartem3412@gmail.com>
2023-02-06 11:36:03 +0300

    edits

--- Bitcoins_app/database/db.py
+++ Bitcoins_app/database/db.py
@@ -2,27 +2,6 @@ from database.models import *
 import models
 from models import *
 
-# db = Database()
-
-
-# class User(db.Entity):
-#     user_id = Required(str)
-#     nick = Required(str)
-#     age = Required(int)
-#     wallets = Set('Wallet')
-
-
-# class Wallet(db.Entity):
-#     address = Required(str)
-#     private_key = Required(str)
-#     owner = Required(User)
-
-
-# try:
-#     db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
-# except Exception as Ex:
-#     print(Ex)
-
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
